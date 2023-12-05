79d911231b497c99cd069b57641a5e1cde03c6da
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2023-12-05 16:57:41 +0630

    2 nd time commit

--- app/controllers/FileController.ts
+++ app/controllers/FileController.ts
@@ -1,6 +1,8 @@
 import Storage from "../storage/index";
 import application from "../constants/application";
-import fs from 'fs';
+ 
+// import csv from 'csvtojson';
+
 
 class FileUpload {
     async uploadFile (req: any, res: any) {
@@ -8,24 +10,53 @@ class FileUpload {
         try {
             const storage = new Storage(application.env.storageLocation)
             const result = await storage.upload(file, "folder") 
-            console.log(result);
              
             const data = JSON.stringify(result);
             const obj = JSON.parse(data);
             const path  = obj.path
-            
-            fetch('url/to/your/csv/'+path)
+            // console.log(path);
 
-            .then(function(response) {
-            return response.text()
-            })
-            // .then(function(csv) {
-            // // convert your csv to an array
-            // });
-                
+            const csv2json = require("./csv2json");
+
+            fetch(path)
+            .then(res => res.text())
+            .then(data => {
+
+            const json = csv2json(data, {parseNumbers: true});
+            // console.log(json);
+            // console.log(json[0].ATB_Status);
+
+            const atbStatusValues = [];
+
+            for (const dataPoint of json) {
+                const atb = dataPoint.ATB_Status;
+                // console.log(`ATB_Status: ${atb}`);
+                atbStatusValues.push(atb);  
+            }
+            // console.log('ATB_Status values:', atbStatusValues);
+
+            //Count ATB_Status times
+            let count = 0;
+
+            for (let i = 0; i < atbStatusValues.length; i++) {
+                // Check if the current value is 1
+                if (atbStatusValues[i] === 1) {
+                    // If it's the first occurrence or the previous value was 0, increment the count
+                    if (i === 0 || atbStatusValues[i - 1] === 0) {
+                    count++;
+                    console.log(`data: ${atbStatusValues[i]} (Start of sequence)`);
+                    }
+                } else {
+                    // If the current value is 0, log it
+                    console.log(`data: ${atbStatusValues[i]} (End of sequence)`);
+                }
+            }
+
+            console.log(`Total ATB_Status: ${count}`);
+            res.status(200).json({TotalATB_Status: `${count}`})
             
-            res.status(200)
-            
+
+            });
             
         } catch (error) {
             console.log(error)
